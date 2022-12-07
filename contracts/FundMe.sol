3ef412fb1f74080513277181a50cbba5c10378c3
ankit patidar <patiankit6555@gmail.com>
2022-12-07 09:05:00 +0530

    6fm

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -15,15 +15,15 @@ contract FundMe {
     using PriceConverter for uint256;
 
     mapping(address => uint256) public addressToAmountFunded;
-    address[] public funders;
+    address[] public  funders;
 
     // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
+    address public  immutable  owner;
     uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
     
     AggregatorV3Interface public priceFeed;
     constructor(address priceFeedAddress) {
-        i_owner = msg.sender;
+        owner = msg.sender;
         priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
@@ -31,7 +31,7 @@ contract FundMe {
         require(msg.value.getConversionRate(priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
         // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+         funders.push(msg.sender);
     }
     
     // function getVersion() public view returns (uint256){
@@ -42,16 +42,16 @@ contract FundMe {
     
     modifier onlyOwner {
         // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        if (msg.sender != owner) revert FundMe__NotOwner();
         _;
     }
     
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
+        for (uint256 funderIndex=0; funderIndex <  funders.length; funderIndex++){
+            address funder =  funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+         funders = new address[](0);
         // // transfer
         // payable(msg.sender).transfer(address(this).balance);
         // // send
